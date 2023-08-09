74d54abc7e96b88671b22e89fdc2955a38922816
Max Bachhuber <max.bachhuber@bahuma.io>
2023-08-09 17:37:03 +0200

    Add link to privacy policy in permission rationale activity.

--- app/src/main/java/io/bahuma/openscale2healthconnect/PermissionsRationaleActivity.kt
+++ app/src/main/java/io/bahuma/openscale2healthconnect/PermissionsRationaleActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.bahuma.openscale2healthconnect.component.HyperlinkText
 import io.bahuma.openscale2healthconnect.ui.theme.OpenScaleToHealthConnectTheme
 
 class PermissionsRationaleActivity : ComponentActivity() {
@@ -17,10 +19,17 @@ class PermissionsRationaleActivity : ComponentActivity() {
             OpenScaleToHealthConnectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text("This app only writes to HealthConnect and does not read anything. It will copy your weight data from OpenScale to Health Connect. Your data will never leave your device or will be uploaded to the internet or a third party.")
+                    HyperlinkText(
+                        fullText = "This app only writes to HealthConnect and does not read anything. It will copy your weight data from OpenScale to Health Connect. Your data will never leave your device or will be uploaded to the internet or a third party.\n\nYou can find more detailed information in our privacy policy.",
+                        hyperLinks = mutableMapOf(
+                            "privacy policy" to "https://bahuma.io/privacy-openscale2healthconnect.php"
+                        )
+                    )
                 }
             }
         }
